89bd60521cf3eec57c8959dee68ea59a5da4348d
tmsnet <tmsnet@126.com>
2024-03-27 20:18:57 +0800

    add more

--- alias.sh
+++ alias.sh
@@ -18,6 +18,17 @@ alias kgp='echo "kgp=kubectl get pods" && kubectl get pods'
 alias kgd='echo "kgd=kubectl get deployments" && kubectl get deployments'
 alias kgs='echo "kgs=kubectl get services" && kubectl get services'
 alias kga='echo "kga=kubectl get all" && kubectl get all'
+alias ka='echo "ka=kubectl apply -f ." && kubectl apply -f .'
+alias kaf='xargs -I {} kubectl apply -f {}'
+
+alias mi='echo "mi=minikube ip" && minikube ip'
+alias ms='echo "ms=minikube start" && minikube start'
+alias mst='echo "mst=minikube stop" && minikube stop'
+alias sb='echo "source ~/.bashrc" && source ~/.bashrc'
+alias na='echo "nano ~/.bash_aliases" && nano ~/.bash_aliases'
+alias nb='echo "nano ~/.bashrc" && nano ~/.bashrc'
+
+alias cm='curl 192.168.49.2:31768'
 
 # Source global definitions
 if [ -f /etc/bashrc ]; then
